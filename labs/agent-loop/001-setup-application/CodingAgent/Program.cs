43a5ba5c70d77685ea5e09cd381c6af3293aedd6
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-11-02 10:08:21 +0100

    Add lab description of the first agent loop lab

---
+++ labs/agent-loop/001-setup-application/CodingAgent/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets(typeof(Program).Assembly)
+    .AddEnvironmentVariables()
+    .Build();
+
+var kernelBuilder = Kernel.CreateBuilder()
+    .AddAzureOpenAIChatCompletion(
+        configuration["LanguageModel:DeploymentName"]!,
+        configuration["LanguageModel:Endpoint"]!,
+        configuration["LanguageModel:ApiKey"]!
+    );
+
+var kernel = kernelBuilder.Build();
+
+Console.WriteLine("You bettah check yoself, you're about to build an agent.");
