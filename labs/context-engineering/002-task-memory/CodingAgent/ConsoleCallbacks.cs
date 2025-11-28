2e247329afa4c6bffcbf21a3704710a2e161ee93
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-11-28 10:07:21 +0100

    Add specialized reporting of TODO items

--- labs/context-engineering/002-task-memory/CodingAgent/ConsoleCallbacks.cs
+++ labs/context-engineering/002-task-memory/CodingAgent/ConsoleCallbacks.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using CodingAgent.Plugins.TaskMemory;
 using Microsoft.SemanticKernel;
 using Spectre.Console;
 
@@ -7,6 +9,12 @@ public class ConsoleCallbacks : IAgentCallbacks
 {
     public Task ReportFunctionCallAsync(string functionName, KernelArguments? arguments, object? output)
     {
+        if (functionName == "write_todos")
+        {
+            ReportTodoItemListCall(JsonSerializer.Deserialize<List<TodoItem>>(arguments!["todos"].ToString()));
+            return Task.CompletedTask;
+        }
+        
         AnsiConsole.Write(new Rule($"Tool Call ({functionName})"));
 
         if(arguments is not null) 
@@ -19,6 +27,27 @@ public class ConsoleCallbacks : IAgentCallbacks
         }
 
         return Task.CompletedTask;
+    }
+
+    private void ReportTodoItemListCall(List<TodoItem> todoItems)
+    {
+        AnsiConsole.Write("Current plan:\n");
+        
+        foreach (var todoItem in todoItems)
+        {
+            if(todoItem.Status.ToLower() == "completed")
+            {
+                AnsiConsole.Write($"- [x] {todoItem.Content}\n");
+            }
+            else if (todoItem.Status.ToLower() == "in_progress")
+            {
+                AnsiConsole.Write(new Markup("[bold]" + Markup.Escape($"- [ ] {todoItem.Content}") + "[/]\n"));
+            }
+            else
+            {
+                AnsiConsole.Write($"- [ ] {todoItem.Content}\n");
+            }
+        }
     }
 
     public Task ReportAgentResponseAsync(string responseContent)
